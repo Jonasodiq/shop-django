e3cc93fc8ac543b10924995d037796db0dc13fdb
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-07-03 12:02:00 +0200

    Add categories

--- main/views.py
+++ main/views.py
@@ -1,20 +1,26 @@
+# from django.db.models import BaseManager
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
+from goods.models import Categories
 
 def index(request):
-    context: dict[str, str] = {
+
+    categories: BaseManager[Categories] = Categories.objects.all()
+
+    context: dict[str, Any] = {
         'title': 'Home - Startsida',
         'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    context: dict[str, str] = {
-        'title': 'Home - Om oss',
-        'content': 'Om oss',
-        'text_on_page': "Text om varför vi är bäst och varför ska du välja oss"
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
     }
 
     return render(request, 'main/about.html', context)
