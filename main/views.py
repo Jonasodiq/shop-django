b4a1c006ea7381327e6ea0c452b21e3ba6968559
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-07-03 19:36:23 +0200

    Register simple tag for categories

--- main/views.py
+++ main/views.py
@@ -2,16 +2,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import context
+
 from goods.models import Categories
 
 def index(request):
 
-    categories: BaseManager[Categories] = Categories.objects.all()
-
     context: dict[str, Any] = {
         'title': 'Home - Startsida',
         'content': 'Магазин мебели HOME',
-        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
