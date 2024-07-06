c3597bb3235976284b91de171bcc091f831acfdc
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-07-06 13:35:28 +0200

    The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,6 @@
+from urllib.parse import urlencode
 from django import template
+from typing import Any
 
 from goods.models import Categories
 
@@ -7,3 +9,12 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs) -> str:
+    # Kopiera GET-parametrarna från request-objektet
+    query: dict[str, Any] = context['request'].GET.copy()
+    # Uppdatera query dict med de nya parametrarna
+    query.update(kwargs)
+    # Returnera den uppdaterade query-strängen
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -1,72 +1,90 @@
-from django.core.paginator import Paginator
-from django.db.models import Manager
-from django.shortcuts import get_list_or_404, render
-
-from goods.models import Products
-from typing import Any
-
-def catalog(request, category_slug):
-
-    page: Any = request.GET.get('page', 1)
-
-    if category_slug == 'all':
-        goods: Manager(Products) = Products.objects.all()
-    else:
-        goods: Any = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-
-    paginator = Paginator(goods, 3)
-    current_page: Page = paginator.page(int(page))
-
-    context: dict[str, Any] = {
-        'title': 'Home - Каталог',
-        'goods': current_page,
-        'slug_url': category_slug,
-    }
-    return render(request, "goods/catalog.html", context)
-
-def product(request, product_slug):
-
-    product: Products = Products.objects.get(slug = product_slug)
-
-    context: dict[str, Products] = {
-        'product': product
-    }
-
-    return render(request, "goods/product.html", context=context)
-
-
-#######
-
-# from django.core.paginator import Paginator, Page
-# from django.db.models import Manager, QuerySet
-# from django.shortcuts import get_list_or_404, render, get_object_or_404
+# from django.core.paginator import Paginator
+# from django.db.models import BaseManager, Manager
+# from django.shortcuts import get_list_or_404, render
 
 # from goods.models import Products
-# from typing import Any, Dict
+# from typing import Any
 
-# def catalog(request, category_slug: str, page: int = 1):
+# def catalog(request, category_slug):
+
+#     page: Any = request.GET.get('page', 1)
+#     on_sale: Any = request.GET.get('on_sale', None)
+#     order_by: Any = request.GET.get('order_by', None)
 
 #     if category_slug == 'all':
-#         goods: QuerySet[Products] = Products.objects.all()
+#         goods: Manager(Products) = Products.objects.all()
 #     else:
-#         goods: QuerySet[Products] = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+#         goods: Any = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+#     if on_sale:
+#         goods: BaseManager[Products] | = goods.filter(discount__qt=0)
+
+#     if order_by and order_by != "default":
+#         goods: BaseManager[Products] | = goods.order_by(order_by)
+    
 #     paginator = Paginator(goods, 3)
-#     current_page: Page = paginator.page(page)
-
-#     context: Dict[str, Any] = {
+#     current_page: Page = paginator.page(int(page))
+ 
+#     context: dict[str, Any] = {
 #         'title': 'Home - Каталог',
 #         'goods': current_page,
 #         'slug_url': category_slug,
 #     }
 #     return render(request, "goods/catalog.html", context)
 
-# def product(request, product_slug: str):
+# def product(request, product_slug):
 
-#     product: Products = get_object_or_404(Products, slug=product_slug)
+#     product: Products = Products.objects.get(slug = product_slug)
 
-#     context: Dict[str, Products] = {
+#     context: dict[str, Products] = {
 #         'product': product
 #     }
 
 #     return render(request, "goods/product.html", context=context)
+
+
+#######
+
+from django.core.paginator import Paginator, Page
+from django.db.models import QuerySet
+from django.shortcuts import get_list_or_404, render, get_object_or_404
+
+from goods.models import Products
+from typing import Any, Dict
+
+def catalog(request, category_slug: str):
+
+    page: int = int(request.GET.get('page', 1))
+    on_sale: str = request.GET.get('on_sale', None)
+    order_by: str = request.GET.get('order_by', None)
+
+    if category_slug == 'all':
+        goods: QuerySet[Products] = Products.objects.all()
+    else:
+        goods: QuerySet[Products] = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+    
+    paginator = Paginator(goods, 3)
+    current_page: Page = paginator.page(page)
+ 
+    context: Dict[str, Any] = {
+        'title': 'Home - Каталог',
+        'goods': current_page,
+        'slug_url': category_slug,
+    }
+    return render(request, "goods/catalog.html", context)
+
+def product(request, product_slug: str):
+
+    product: Products = get_object_or_404(Products, slug=product_slug)
+
+    context: Dict[str, Products] = {
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
