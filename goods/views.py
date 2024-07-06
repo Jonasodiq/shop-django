245b3a87b8df612cd5398e48395082b5b79ed442
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-07-06 19:26:40 +0200

    Search urls & views added

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,54 @@
+from django.core.paginator import Paginator, Page
+from django.db.models import QuerySet
+from django.shortcuts import get_list_or_404, render, get_object_or_404
+
+from goods.models import Products
+from typing import Any, Dict
+
+from goods.utils import q_search
+
+def catalog(request, category_slug=None):
+
+    page: int = int(request.GET.get('page', 1))
+    on_sale: str = request.GET.get('on_sale', None)
+    order_by: str = request.GET.get('order_by', None)
+    query: str = request.GET.get('q', None)
+
+    if category_slug == 'all':
+        goods: QuerySet[Products] = Products.objects.all()
+    elif query:
+        goods: Any = q_search(query)
+    else:
+        goods: QuerySet[Products] = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+    
+    paginator = Paginator(goods, 3)
+    current_page: Page = paginator.page(page)
+ 
+    context: Dict[str, Any] = {
+        'title': 'Home - Каталог',
+        'goods': current_page,
+        'slug_url': category_slug,
+    }
+    return render(request, "goods/catalog.html", context)
+
+def product(request, product_slug: str):
+
+    product: Products = get_object_or_404(Products, slug=product_slug)
+
+    context: Dict[str, Products] = {
+        'product': product
+    }
+
+    return render(request, "goods/product.html", context=context)
+
+#######
+
 # from django.core.paginator import Paginator
 # from django.db.models import BaseManager, Manager
 # from django.shortcuts import get_list_or_404, render
@@ -44,47 +95,3 @@
 
 
 #######
-
-from django.core.paginator import Paginator, Page
-from django.db.models import QuerySet
-from django.shortcuts import get_list_or_404, render, get_object_or_404
-
-from goods.models import Products
-from typing import Any, Dict
-
-def catalog(request, category_slug: str):
-
-    page: int = int(request.GET.get('page', 1))
-    on_sale: str = request.GET.get('on_sale', None)
-    order_by: str = request.GET.get('order_by', None)
-
-    if category_slug == 'all':
-        goods: QuerySet[Products] = Products.objects.all()
-    else:
-        goods: QuerySet[Products] = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-
-    if on_sale:
-        goods = goods.filter(discount__gt=0)
-
-    if order_by and order_by != "default":
-        goods = goods.order_by(order_by)
-    
-    paginator = Paginator(goods, 3)
-    current_page: Page = paginator.page(page)
- 
-    context: Dict[str, Any] = {
-        'title': 'Home - Каталог',
-        'goods': current_page,
-        'slug_url': category_slug,
-    }
-    return render(request, "goods/catalog.html", context)
-
-def product(request, product_slug: str):
-
-    product: Products = get_object_or_404(Products, slug=product_slug)
-
-    context: Dict[str, Products] = {
-        'product': product
-    }
-
-    return render(request, "goods/product.html", context=context)
